dc6480dff083c45f2a3ae6ea64972e3bd3c2c8f6
Karaoglan <burak.karaoglan>
2022-03-18 09:44:36 +0300

    check inner html

--- src/main/resources/client/src/components/Card.tsx
+++ src/main/resources/client/src/components/Card.tsx
@@ -14,7 +14,7 @@ export const Card: FunctionComponent<H2NPost> = ({ id, summarizeText, clickHandl
         className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
         <div className="mb-8">
           <div className="text-gray-900 font-bold text-xl mb-2">{id}</div>
-          <p className="text-gray-700 text-base">{summarizeText}</p>
+          <p className="text-gray-700 text-base" dangerouslySetInnerHTML={{__html: summarizeText}}></p>
         </div>
         <div className="flex items-center">
           <img className="w-10 h-10 rounded-full mr-4" src="https://v1.tailwindcss.com/img/jonathan.jpg" alt="Avatar of Jonathan Reinink"/>
