e124dd51a0b8854dfd7ea1ca4894682fad19eddf
burak.karaoglan <burakkaraoglan55@gmail.com>
2022-03-29 06:11:22 +0300

    md design

--- src/main/resources/client/src/App.tsx
+++ src/main/resources/client/src/App.tsx
@@ -50,7 +50,7 @@ function App() {
   const postsElem = () => {
     return <div className="">
       {!posts.length ? <CardPulseLoading/> : <></>}
-      <div className="grid grid-cols-3 gap-20">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
         {posts.map((post, idx) => (
           <Card clickHandler={() => clicked(post.id)} key={idx} id={post.id} postText={''}
                 summarizeText={post.summarizeText}/>
@@ -58,41 +58,6 @@ function App() {
       </div>
     </div>
   }
-
-  /*const postsElem = () => {
-    return <div className="">
-      {!posts.length ? <CardPulseLoading/> : <></>}
-      <div className="grid grid-cols-3 gap-3">
-      {posts.map((post, idx) => (
-
-          <div onClick={() => clicked(post.id)} className="w-72">
-            <div
-              className="card-image h-36 lg:h-auto lg:w-36 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-              title="Woman holding a mug">
-            </div>
-            <div
-              className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-              <div className="mb-8">
-                <div
-                  className="text-gray-900 font-bold text-xl mb-2">{post.id} {post.summarizeText ? ' - ' + post.summarizeText : ''}</div>
-                <p className="text-gray-700 text-base" dangerouslySetInnerHTML={{__html: post.postText}}></p>
-              </div>
-              <div className="flex items-center">
-                <img className="w-10 h-10 rounded-full mr-4" src="https://v1.tailwindcss.com/img/jonathan.jpg"
-                     alt="Avatar of Jonathan Reinink"/>
-                <div className="text-sm">
-                  <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-                  <p className="text-gray-600">Aug 18</p>
-                </div>
-              </div>
-            </div>
-          </div>
-      ))}
-      </div>
-
-    </div>
-  }*/
-
 
   const findPostText = (id: string): string => {
     const post = posts.find(post => post.id === id);
--- src/main/resources/client/src/components/NavBar.tsx
+++ src/main/resources/client/src/components/NavBar.tsx
@@ -7,14 +7,14 @@ export const NavBar: FunctionComponent = () => {
     const [collapse, setCollapse] = useState(false);
 
     return (
-        <aside className="h-screen sticky top-0 m-10 w-64 overflow-y-auto py-4 px-3 rounded" aria-label="Sidebar">
+        <aside className="h-screen sticky top-0 m-2 w-64 overflow-y-auto py-4 px-3 lg:m-10 rounded" aria-label="Sidebar">
                 <ul className="space-y-2">
                     <li>
                         <Link to="/">
                             <a href="#"
-                               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-gray-900 hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-gray-900 hover:text-black hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <svg
-                                    className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                    className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900"
                                     fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                                     <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
@@ -26,16 +26,16 @@ export const NavBar: FunctionComponent = () => {
                     <li>
                         <button type="button"
                                 onClick={() => setCollapse(!collapse)}
-                                className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:hover:bg-gray-700"
                                 aria-controls="dropdown-example">
                             <svg
-                                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                       d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
                                       clip-rule="evenodd"></path>
                             </svg>
-                            <span className="flex-1 ml-3 text-gray-900 text-left whitespace-nowrap hover:text-white"
+                            <span className="flex-1 ml-3 text-gray-900 text-left whitespace-nowrap"
                                   >E-commerce</span>
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                  xmlns="http://www.w3.org/2000/svg">
@@ -47,21 +47,21 @@ export const NavBar: FunctionComponent = () => {
                         <ul className={(!collapse ? "hidden" : "") + " py-2 space-y-2"}>
                             <li>
                                 <a href="#"
-                                   className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-white dark:hover:bg-gray-700">Products</a>
+                                   className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:hover:bg-gray-700">Products</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-white dark:hover:bg-gray-700">Billing</a>
+                                   className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:hover:bg-gray-700">Billing</a>
                             </li>
                             <li>
                                 <a href="#"
-                                   className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-white dark:hover:bg-gray-700">Invoice</a>
+                                   className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:hover:bg-gray-700">Invoice</a>
                             </li>
                         </ul>
                     </li>
                     <li>
                         <a href="#"
-                           className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-white">
+                           className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-gray-700">
                             <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
                         </a>
                     </li>
