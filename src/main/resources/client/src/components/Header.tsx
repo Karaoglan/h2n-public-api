6e0e779c947c7f809540402744b1e3d1fc167ff7
Karaoglan <burak.karaoglan>
2022-04-28 10:46:26 +0300

    add collapse search icon

--- src/main/resources/client/src/components/Header.tsx
+++ src/main/resources/client/src/components/Header.tsx
@@ -3,7 +3,6 @@ import worldIcon from "../assets/icons/world.svg";
 import searchIcon from "../assets/icons/search.svg";
 import sidebarActiveIcon from "../assets/icons/sidebarActive.svg";
 import closeSidebarIcon from "../assets/icons/closeSidebar.svg";
-import {StringBuilder} from "../utils/StringBuilder";
 
 type Props = {
   currentLang: string,
@@ -16,10 +15,17 @@ export const HeaderPage: FunctionComponent<Props> = ({currentLang, setLang, hidd
   const inputReference = useRef<HTMLInputElement>(null);
   const [focus, setFocus] = useState(false);
   const [languages, setLanguages] = useState(['DE', 'TR', 'ENG']);
+  const [searchIconClicked, setSearchIconClicked] = useState(false);
 
   useEffect(() => {
     setLanguages(languages.sort((a, b) => currentLang === a ? -1 : 1));
   }, [currentLang]);
+
+  useEffect(() => {
+    if (searchIconClicked) {
+      changeFocus(true);
+    }
+  }, [searchIconClicked])
 
   const changeFocus = (focus: boolean) => {
     setFocus(focus);
@@ -28,26 +34,9 @@ export const HeaderPage: FunctionComponent<Props> = ({currentLang, setLang, hidd
     }
   }
 
-  const inputClasses = StringBuilder.Builder()
-    .append("rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline")
-    .append(focus ? 'bg-white shadow appearance-none border' : '')
-    .build();
-
   const active = (lang: string) => {
     return lang === currentLang ? 'font-black' : '';
   }
-  /*
-  <img alt="search-icon" onClick={() => changeFocus(true)} src={searchIcon}
-             className="w-4 h-4 cursor-pointer "/>
-        <form className="ml-2 grow">
-          <input
-            onBlur={() => changeFocus(false)}
-            ref={inputReference}
-            className={inputClasses}
-            id="username" type="text"
-            placeholder={focus ? 'Ara ...' : ''}/>
-        </form>
-   */
 
   return (
     <>
@@ -58,17 +47,20 @@ export const HeaderPage: FunctionComponent<Props> = ({currentLang, setLang, hidd
         {window.innerWidth <= 650 && !hiddenSidebar &&
           <img alt="sidebar-inactive-icon" onClick={collapseSidebar} src={closeSidebarIcon}
                className="w-4 h-4 cursor-pointer hover:bg-gray-300"/>}
-        {'replace- here'}
-        <div className="flex justify-between py-3 px-6 bg-gray-50 border-b w-0 hover:w-64 transition-all duration-700">
+        <div className={`flex justify-between ${searchIconClicked ? 'w-64' : 'w-0'} transition-all duration-700`}>
+          <form onSubmit={e => {
+            e.preventDefault();
+            setSearchIconClicked(false);
+          }}>
 
-          <form className="">
-
-            <div className="relative flex items-center text-gray-400">
+            <div className={`relative flex flex-row-reverse items-center text-gray-400`}>
               <img alt="search-icon" src={searchIcon}
+                   onClick={() => setSearchIconClicked(!searchIconClicked)}
                    className="w-4 h-4 cursor-pointer absolute ml-3"/>
               <input
                 type="text"
-                className="pr-3 pl-10 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white shadow appearance-none border"
+                ref={inputReference}
+                className={`rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white appearance-none ${searchIconClicked ? 'border-b' : ''}`}
                 placeholder='Ara ...'/>
             </div>
           </form>
